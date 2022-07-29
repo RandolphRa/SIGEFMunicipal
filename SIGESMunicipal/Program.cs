88a9066724a59ba282a521658749f3e11a566e26
teresavargas19 <vargasbrito19@gmail.com>
2022-07-29 08:54:29 -0400

    iis

--- SIGESMunicipal/Program.cs
+++ SIGESMunicipal/Program.cs
@@ -1,37 +1,9 @@
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://www.sigefmunicipal.gob.do/",
-                                              "http://www.sigefmunicipalfront.gob.do/");
-
-                      });
-});
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
-app.UseCors();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGet("/echo",
-        context => context.Response.WriteAsync("echo"))
-        .RequireCors(MyAllowSpecificOrigins);
-
-    endpoints.MapControllers()
-             .RequireCors(MyAllowSpecificOrigins);
-
-    endpoints.MapGet("/echo2",
-        context => context.Response.WriteAsync("echo2"));
-
-    endpoints.MapRazorPages();
-});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
